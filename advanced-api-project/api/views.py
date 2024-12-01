8a470f9b4df032563712fc6bb3f4899365c26384
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2024-12-01 23:13:43 +0100

    Fifth commit on advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, serializers, permissions, filters
 from django_filters import rest_framework
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import Book
 from .serializers import BookSerializer
