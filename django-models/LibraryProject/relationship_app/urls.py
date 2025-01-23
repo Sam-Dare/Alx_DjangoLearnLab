153c8792d1507a3a7ef0c7623ca29ceb53de8bf8
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2025-01-23 08:46:42 +0100

    Sixth commit for django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
+from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView  # Importing LoginView and LogoutView
 
 urlpatterns = [
     # Book and Library URLs
-    path('books/add/', views.add_book, name='add_book'),  # URL for adding books
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),  # URL for editing books
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),  # URL for deleting books
-    path('books/', views.list_books, name='list_books'),  # FBV for listing books
+    path('books/add_book/', views.add_book, name='add_book'),  # URL for adding books
+    path('books/<int:pk>/edit_book/', views.edit_book, name='edit_book'),  # URL for editing books
+    path('books/<int:pk>/delete_book/', views.delete_book, name='delete_book'),  # URL for deleting books
+    path('books/', list_books, name='list_books'),  # FBV for listing books
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV for library details
 
     # User Authentication URLs
