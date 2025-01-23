b4f1c30e27b5c6bd7344ebd392cbe0c70e82ba33
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2025-01-23 08:40:10 +0100

    Fifth commit for django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, login_required, user_passes_test
 from django.views.generic.detail import DetailView
-from .models import Book, Library, UserProfile
+from .models import Library, Book, UserProfile
 from .forms import BookForm  # Assuming you have a form for handling Book objects
 
 
