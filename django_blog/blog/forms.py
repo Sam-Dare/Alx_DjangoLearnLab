f8cd17bf4ae3921c6560ce37271c67be1a4d11d3
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2025-01-24 09:03:30 +0100

    Eleventh commit for django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,10 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from taggit.forms import TagWidget
-
 from .models import Post, Comment
-from .models import Post  # Adjust based on your models
-from taggit.forms import TagWidget 
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -25,7 +22,7 @@ class CommentForm(forms.ModelForm):
         fields = ['content']
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(widget=TagWidget(attrs={'placeholder': 'Add tags, separated by commas'}))
+    tags = forms.CharField(widgets=TagWidget(attrs={'placeholder': 'Add tags, separated by commas'}))
 
     class Meta:
         model = Post
